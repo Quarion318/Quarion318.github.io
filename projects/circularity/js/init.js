67732f664b644fd4dea4a61b58ae153a35b10791
Quarion318 <quarion.davis.student@madisonpsb.org>
2023-05-18 16:12:54 +0000

    circularity

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,13 +20,25 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+        var circle;
+        var circles = [];
         // TODO 2 : Create a function that draws a circle 
-        
-
+        function drawCircle () {
+            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+        }
+              
+        for (var i = 0; i < 100; i++){
+            drawCircle()
+        }
         // TODO 3 / 7 : Call the drawCircle() function 
+           
 
+           
+           
+        
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
@@ -39,10 +51,16 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
+          for (var i = 0; i <= circles.length; i++){
+            physikz.updatePosition(circles[i]);
+            game.checkCirclePosition(circles[i]);
+          }
+        
+           
+            
             
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            
 
             // TODO 9 : Iterate over the array
            
@@ -62,9 +80,13 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circle.x < 0){
+                circle.x = canvas.width;
+            }
 
-
+            if (canvas.y > canvas.height){
+                circle.y = canvas.height
+            }
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
